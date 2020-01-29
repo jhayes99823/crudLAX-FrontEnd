52b290a69a8104c9d606307e55b981ffa2e97060
Jordan Hayes <hayesja99@gmail.com>
2020-01-28 21:26:13 -0500

    add some basic player stuff

---
+++ src/pages/Player/HomePage.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import NavBar from '../../components/NavBar';
+import Table from '../../components/Table';
+
+export default function PlayerHomePage() {
+    return (
+        <div>
+            <NavBar />
+            <Table />
+        </div>
+    )
+}
