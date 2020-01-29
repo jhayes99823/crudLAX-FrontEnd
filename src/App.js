431d9aca7557f2e2e88915e23b9e135af6a46d71
Jordan Hayes <hayesja99@gmail.com>
2020-01-29 02:00:02 -0500

    setup file structure for josh

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar/NavBar.component';
 import InformationTable from './components/Table/Table.component';
+
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
+
+import Login from './components/LogIn/login.component';
+import Signup from './components/SignUp/signup.component';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <InformationTable />
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={Signup} />
+          </Switch>
+        </Router>
       </div>
     );
   }
