0508d197d08c4f2dbc18ea8c4313e18011cb3787
Jordan Hayes <hayesja99@gmail.com>
2020-02-13 21:03:46 -0500

    new roster comps

---
+++ src/components/FindPlayer/FindPlayer.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class FindPlayer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Find Player Comp
+            </div>
+        )
+    }
+}
